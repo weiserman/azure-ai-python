ebff434689bd4e5ff66147548226b4f25ae9dd0a
skullquake <ockert8080@gmail.com>
2025-08-22 14:37:24 +0200

    test script
    Signed-off-by: skullquake <ockert8080@gmail.com>

--- test_local.sh
+++ test_local.sh
@@ -2,6 +2,11 @@
 PROTOCOL=http
 HOST=localhost
 PORT=7071
+# Generate temp files for the images
+TMP1=$(mktemp /tmp/attach1.XXXXXX.png)
+TMP2=$(mktemp /tmp/attach2.XXXXXX.png)
+convert rose: -resize 1x1 "$TMP1"
+convert rose: -resize 1x1 "$TMP2"
 <<COMMENT_BLOCK
 COMMENT_BLOCK
 curl -v -X POST "$PROTOCOL://$HOST:$PORT/api/callai" \
@@ -11,16 +16,16 @@ curl -v -X POST "$PROTOCOL://$HOST:$PORT/api/callai" \
     "subject": "Test Subject",
     "recipient": "test@example.com",
     "attachments":[
-    	"'$(convert rose: --format png - |base64 -w0)'",
-    	"'$(convert rose: --format png - |base64 -w0)'"
+    	"'$(convert rose: -resize 1x1 --format png - |base64 -w0)'",
+    	"'$(convert rose: -resize 1x1 --format png - |base64 -w0)'"
     ],
     "body": "This is a test email body."
   }' | jq '.'
 curl -X POST "$PROTOCOL://$HOST:$PORT/api/callai" \
-    -F "header=Test Header" \
-    -F "subject=Test Subject" \
-    -F "recipient=test@example.com" \
-    -F "body=This is a test email body." \
-    -F "attachments=@./a.png" \
-    -F "attachments=@./b.png" \
-    -F "attachments=@./c.png" | jq '.'
+  -F "header=Test Header" \
+  -F "subject=Test Subject" \
+  -F "recipient=test@example.com" \
+  -F "body=This is a test email body." \
+  -F "attachments=@$TMP1" \
+  -F "attachments=@$TMP2" | jq '.'
+rm -f "$TMP1" "$TMP2"
